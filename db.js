e302f269f0930fba0209d0230d56321e8f2bb518
Manishroy77 <kashyapmanu8877@mk.com>
2024-11-28 13:34:20 +0530

    changes mongodb to online

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 // const mongoUrl = process.env.mongoUrlLocal;
 const mongoUrl = process.env.mongoUrlOnline;
 mongoose.connect(mongoUrl);
